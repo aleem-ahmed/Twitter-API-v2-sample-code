7b4fbc47a0bdb4fb432e51930b23ae66898372aa
aleem ahmed <61424530+aleem-ahmed@users.noreply.github.com>
2020-12-07 17:32:45 -0500

    Lets Make This Easier to Read
    Simple syntax fixes

--- Filtered-Stream/filtered_stream.js
+++ Filtered-Stream/filtered_stream.js
@@ -3,22 +3,21 @@ const needle = require('needle');
 // The code below sets the bearer token from your environment variables
 // To set environment variables on Mac OS X, run the export command below from the terminal: 
 // export BEARER_TOKEN='YOUR-TOKEN'
-const token = process.env.BEARER_TOKEN;  
+const token = process.env.BEARER_TOKEN;
 
-const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
 const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
 
 // Edit rules as desired here below
 const rules = [
-    { 'value': 'dog has:images -is:retweet', 'tag': 'dog pictures' }, 
+    { 'value': 'dog has:images -is:retweet', 'tag': 'dog pictures' },
     { 'value': 'cat has:images -grumpy', 'tag': 'cat pictures' },
-  ];
+];
 
 async function getAllRules() {
-
-    const response = await needle('get', rulesURL, { headers: {
-        "authorization": `Bearer ${token}`
-    }})
+    const response = await needle('get', rulesURL, {
+        headers: { "authorization": `Bearer ${token}` }
+    });
 
     if (response.statusCode !== 200) {
         throw new Error(response.body);
@@ -29,10 +28,9 @@ async function getAllRules() {
 }
 
 async function deleteAllRules(rules) {
-
     if (!Array.isArray(rules.data)) {
         return null;
-      }
+    }
 
     const ids = rules.data.map(rule => rule.id);
 
@@ -40,12 +38,14 @@ async function deleteAllRules(rules) {
         "delete": {
             "ids": ids
         }
-    }
+    };
 
-    const response = await needle('post', rulesURL, data, {headers: {
-        "content-type": "application/json",
-        "authorization": `Bearer ${token}`
-    }}) 
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            "content-type": "application/json",
+            "authorization": `Bearer ${token}`
+        }
+    });
 
     if (response.statusCode !== 200) {
         throw new Error(response.body);
@@ -53,19 +53,17 @@ async function deleteAllRules(rules) {
     }
     
     return (response.body);
-
 }
 
 async function setRules() {
-
     const data = {
         "add": rules
-      }
+    };
 
     const response = await needle('post', rulesURL, data, {headers: {
         "content-type": "application/json",
         "authorization": `Bearer ${token}`
-    }}) 
+    }});
 
     if (response.statusCode !== 201) {
         throw new Error(response.body);
@@ -73,14 +71,13 @@ async function setRules() {
     }
     
     return (response.body);
-
 }
 
 function streamConnect() {
     //Listen to the stream
     const options = {
         timeout: 20000
-      }
+    };
     
     const stream = needle.get(streamURL, {
         headers: { 
@@ -92,7 +89,8 @@ function streamConnect() {
     try {
         const json = JSON.parse(data);
         console.log(json);
-    } catch (e) {
+    }
+    catch (e) {
         // Keep alive signal received. Do nothing.
     }
     }).on('error', error => {
@@ -102,9 +100,7 @@ function streamConnect() {
     });
 
     return stream;
-    
 }
-
 
 (async () => {
     let currentRules;
@@ -118,8 +114,8 @@ function streamConnect() {
   
       // Add rules to the stream. Comment the line below if you don't want to add new rules.
       await setRules();
-      
-    } catch (e) {
+    }
+    catch (e) {
       console.error(e);
       process.exit(-1);
     }
@@ -129,16 +125,17 @@ function streamConnect() {
     // To avoid rate limites, this logic implements exponential backoff, so the wait time
     // will increase if the client cannot reconnect to the stream.
   
-    const filteredStream = streamConnect()
+    const filteredStream = streamConnect();
     let timeout = 0;
-    filteredStream.on('timeout', () => {
-      // Reconnect on error
-      console.warn('A connection error occurred. Reconnecting…');
-      setTimeout(() => {
-        timeout++;
-        streamConnect(token);
-      }, 2 ** timeout);
-      streamConnect(token);
-    })
 
-  })();
+    filteredStream.on('timeout', () => {
+        // Reconnect on error
+        console.warn('A connection error occurred. Reconnecting...');
+
+        setTimeout(() => {
+            timeout++;
+            streamConnect(token);
+        }, 2 ** timeout);
+        streamConnect(token);
+    });
+})();
