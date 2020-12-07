3eef2f1dbd43073fa0acb8e7adac23f1bcb30739
aleem ahmed <61424530+aleem-ahmed@users.noreply.github.com>
2020-12-07 17:48:02 -0500

    Making It Easier to Read for Noobs
    small syntax fixes

--- Recent-Search/recent_search.js
+++ Recent-Search/recent_search.js
@@ -5,37 +5,38 @@ const needle = require('needle');
 // export BEARER_TOKEN='YOUR-TOKEN' 
 const token = process.env.BEARER_TOKEN; 
 
-const endpointUrl = 'https://api.twitter.com/2/tweets/search/recent'
+const endpointUrl = 'https://api.twitter.com/2/tweets/search/recent';
 
 async function getRequest() {
-
     // Edit query parameters below
     const params = {
         'query': 'from:twitterdev -is:retweet', 
         'tweet.fields': 'author_id' 
-    } 
+    };
 
-    const res = await needle('get', endpointUrl, params, { headers: {
-        "authorization": `Bearer ${token}`
-    }})
+    const res = await needle('get', endpointUrl, params, {
+        headers: {
+            "authorization": `Bearer ${token}`
+        }
+    });
 
-    if(res.body) {
+    if (res.body) {
         return res.body;
-    } else {
+    }
+    else {
         throw new Error ('Unsuccessful request')
     }
 }
 
 (async () => {
-
     try {
         // Make request
         const response = await getRequest();
-        console.log(response)
-
-    } catch(e) {
+        console.log(response);
+    }
+    catch(e) {
         console.log(e);
         process.exit(-1);
     }
     process.exit();
-  })();
+})();
